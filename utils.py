b0367716d4963a583cc365b9a5c8f5d1da84a034
踢低吸 <tasi788@gmail.com>
2018-09-14 21:54:21 +0800

    update

--- utils.py
+++ utils.py
@@ -1,4 +1,6 @@
 from dateutil.parser import parse as dateParse
+from PIL import Image,ImageDraw,ImageFont
+import telepot
 
 def tformat(rawTime):
 	# 2018年8月23日 5:30 PM
@@ -9,3 +11,10 @@ def tformat(rawTime):
 	for x, y in zip(['年', '月', '日', ''], parse_):
 		tmp += y + x
 	return tmp
+
+def splitLine(world):
+	im = Image.new('RGB', (60, 30), color = 'red')
+	draw = ImageDraw.Draw(im)
+	msg = f'{world}'.encode('utf-8')
+	w,h = draw.textsize(msg)
+	return '-'*round(w/6)+'-'
