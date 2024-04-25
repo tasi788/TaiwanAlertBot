e7b26853684adfbee857be5055fb204c060f7b2c
tdc <tdc@tdccc.com.tw>
2024-04-25 17:59:20 +0800

    ⚡️ 修改了邏輯同時新增通用警報

---
+++ src/generator/defaultgen.ts
@@ -0,0 +1,19 @@
+import {
+    AlertRoot, GeneratorText
+} from '../types';
+
+export async function defaultgen(report: AlertRoot): Promise<GeneratorText> {
+    let info = report.alert.info
+    let color = info.parameter.map((p) => p.valueName === 'alert_color' ? p.value : '').join('')
+    if (color === '') {
+        color = info.parameter.map((p) => p.valueName === 'alert_title' ? p.value : '').join('')
+    }
+    let text = `發布單位：#${info.senderName}\n` +
+               `警報活動：#${info.event}\n`+
+               `警報顏色：#${color}\n` +
+               `警報標題：${info.headline}\n\n` +
+               `警報描述：${info.description}\n\n` +
+               `警報發布時間：${info.effective.getFullYear()}年 ${(info.effective.getMonth() + 1)}月 ${info.effective.getDate()}日 ${info.effective.getHours()}:${info.effective.getMinutes()}\n` +
+               `警報過期時間：${info.expires.getFullYear()}年 ${(info.expires.getMonth() + 1)}月 ${info.expires.getDate()}日 ${info.expires.getHours()}:${info.expires.getMinutes()}\n\n`
+    return new GeneratorText(text, []);
+}
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,23 @@
-import { XMLParser } from 'fast-xml-parser';
-import { AlertRoot } from './types';
+import {
+	XMLParser
+} from 'fast-xml-parser';
+import {
+	AlertRoot,
+	GeneratorText
+} from './types';
 
-import { Telegram } from './telegram';
-import { ExecutionContext } from '@cloudflare/workers-types/experimental';
-import { earthquake } from './generator/earthquake';
+import {
+	Telegram
+} from './telegram';
+import {
+	ExecutionContext
+} from '@cloudflare/workers-types/experimental';
+import {
+	earthquake
+} from './generator/earthquake';
+import {
+	defaultgen
+} from './generator/defaultgen';
 
 export interface Env {
 	BOTTOKEN: string;
@@ -22,7 +36,7 @@ export default {
 		if (request.method !== 'POST' || url.pathname != `/${env.WEBHOOK_PATH}`) {
 			return new Response(randomKaomoji);
 		}
-		
+
 		const body = await request.text();
 		let report = await this.parse(env, body);
 		await this.broadcast(env, report);
@@ -36,41 +50,64 @@ export default {
 			"category": {
 				"Health": 1423,
 				"Geo": 1439,
+				"Met": 1928,
 			},
+			// 獨立 topics
 			"event": {
 				"地震": 678
 			}
 		}
 		let text = "";
 		let msg_id = 0;
+		let context: GeneratorText;
+		//  先發到所有警報區 
 		switch (report.alert.info.event) {
 			case '地震':
-				let context = await earthquake(report)
+				context = await earthquake(report)
 				msg_id = await bot.sendMediaGroup(env.CHATID, context.image, context.text, gather)
-				
+
 			default:
-				break;
+				context = await defaultgen(report)
+				msg_id = await bot.sendMessage(env.CHATID, context.text, gather)
 		}
+
+		let topic_copylist: number[] = [];
 		for (let category in topicList.category) {
 			if (category === report.alert.info.category) {
 				let topic: number = topicList.category[category as keyof typeof topicList.category];
-				await bot.copyMessages(env.CHATID, env.CHATID, msg_id, topic)
+				topic_copylist.push(topic);
 			}
 		}
 
 		for (let event in topicList.event) {
 			if (event === report.alert.info.event) {
 				let topic: number = topicList.event[event as keyof typeof topicList.event];
-				await bot.copyMessages(env.CHATID, env.CHATID, msg_id, topic)
+				topic_copylist.push(topic);
 			}
 		}
-		
 
-		// await bot.sendMessage(env.CHATID, text, 1);
+		console.log(topic_copylist)
+		console.log(msg_id)
+		
+		//  批次複製訊息
+		for (let target_topics of topic_copylist) {
+			if (context.image.length > 0) {
+				//  地震圖
+				await bot.copyMessages(env.CHATID, env.CHATID, [msg_id, msg_id + 1], target_topics)
+			}
+			await bot.copyMessage(env.CHATID, env.CHATID, msg_id, target_topics)
+		}
 	},
 
-	async parse(env: Env, context: string): Promise<AlertRoot> {
-		const parser = new XMLParser();
+	async parse(env: Env, context: string): Promise < AlertRoot > {
+		const parser = new XMLParser({
+			tagValueProcessor: (attrName, val) => {
+				if (['effective', 'onset', 'expires'].includes(attrName) === true) {
+					return new Date(val);
+				}
+				return val;
+			}
+		});
 		return parser.parse(context) as AlertRoot;
 	}
 };
